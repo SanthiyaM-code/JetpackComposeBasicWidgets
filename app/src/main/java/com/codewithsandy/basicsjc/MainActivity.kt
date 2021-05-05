0b24eb406157d5ebe51d8e11e67bbb75eed9b077
SanthiyaM-code <santhiya10082001@gmail.com>
2021-05-05 15:25:18 +0530

    layouts

--- app/src/main/java/com/codewithsandy/basicsjc/MainActivity.kt
+++ app/src/main/java/com/codewithsandy/basicsjc/MainActivity.kt
@@ -3,25 +3,52 @@ package com.codewithsandy.basicsjc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codewithsandy.basicsjc.ui.theme.BasicsJCTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting()
+            NewStory()
         }
+    }
+}
+
+@Composable
+fun NewStory()
+{
+    Column(
+        modifier= Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.jcimage),
+            contentDescription = null,
+            modifier=Modifier.height(180.dp).fillMaxWidth(),
+            contentScale= ContentScale.Crop
+        )
+        Spacer(Modifier.height(18.dp))
+
+        Text("Sun hide behind the rocks!")
+        Text("Evenin' reflection falls on he muddy sand!")
+        Text("pleasent wind blows!")
     }
 }
 
 @Preview
 @Composable
-fun Greeting()
+fun DefaultPreview()
 {
-    Text("Welcome")
+    NewStory()
 }
